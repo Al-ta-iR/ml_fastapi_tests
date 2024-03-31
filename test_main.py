ee56645ec34fc44c5bb2f3396739faa04694b64e
Altair <artyom.oleynik@gmail.com>
2024-03-31 20:55:56 +0200

    Add tests to verify handling of empty or invalid input text

--- test_main.py
+++ test_main.py
@@ -22,3 +22,15 @@ def test_predict_negative():
     json_data = response.json()
     assert response.status_code == 200
     assert json_data["label"] == "NEGATIVE"
+
+
+def test_predict_empty_text():
+    response = client.post("/predict/", json={"text": ""})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data["label"] == "NEUTRAL"
+
+
+def test_predict_invalid_text():
+    response = client.post("/predict/", json={"text": 123})
+    assert response.status_code == 422
